027bf25f704487f1d9c35c4b99bd17730c3deedb
sebduret <sebastienduret@msn.com>
2015-04-08 10:49:17 +0200

    init process client

--- MailSMTP/src/client/Client.java
+++ MailSMTP/src/client/Client.java
@@ -1,18 +1,20 @@
 package client;
 
+import java.awt.List;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class Client {
     private final Map<String,String> listDomain;
-    private Map<String,Integer> infoDestination;
+    private Map<String,ArrayList<String>> infoDestination;
     
     public Client(){
         this.listDomain = new HashMap<String,String>();
         this.listDomain.put("toto.fr", "127.0.0.1:2009");
         
-        this.infoDestination = new HashMap<String,Integer>();
+        this.infoDestination = new HashMap<String,ArrayList<String>>();
     }
     
     private void getInfoDestination(){
@@ -21,6 +23,8 @@ public class Client {
     
     public void start(){
         this.getInfoDestination();
+        
+        
         
     }
 }
